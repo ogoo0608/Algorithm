421c7b29cfad2ffbf2a0cd497979ad8618d3ce45
ogoo0608 <ogoo0608@gmail.com>
2022-11-21 10:49:00 +0000

    킹, 퀸, 룩, 비숍, 나이트, 폰

--- Input - output and normal operation/3003.cpp
+++ Input - output and normal operation/3003.cpp
@@ -20,4 +20,33 @@
 */
 
 #include <iostream>
-
+ 
+using namespace std;
+ 
+int main() {
+ 
+	int king = 1;
+	int queen = 1;
+	int rook = 2;
+	int bishop = 2;
+	int knight = 2;
+	int pawn = 8;
+ 
+	int in_king, in_queen, in_rook, in_bishop, in_knight, in_pawn;
+ 
+	cin >> in_king;
+	cin >> in_queen;
+	cin >> in_rook;
+	cin >> in_bishop;
+	cin >> in_knight;
+	cin >> in_pawn;
+ 
+	cout << king - in_king << " ";
+	cout << queen - in_queen << " ";
+	cout << rook - in_rook << " ";
+	cout << bishop - in_bishop << " ";
+	cout << knight - in_knight << " ";
+	cout << pawn - in_pawn << " ";
+ 
+	return 0;
+}
