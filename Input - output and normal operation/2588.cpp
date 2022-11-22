24033738aad76b429c1c0ddafbb3099143b868f2
ogoo0608 <ogoo0608@gmail.com>
2022-11-22 10:25:42 +0000

    곱셈

--- Input - output and normal operation/2588.cpp
+++ Input - output and normal operation/2588.cpp
@@ -12,12 +12,20 @@
 */
 
 #include <iostream>
-#include <array>
 
 using namespace std;
 
 int main() {
 	
+	int a, b;
+	int c[4] = {};
 	
-	return 0;
+	cin >> a >> b;
+
+	c[0] = a * (b % 10);
+	c[1] = a * (b % 100 / 10);
+	c[2] = a * (b / 100);
+	c[3] = a * b;
+
+	cout << c[0] << endl << c[1] << endl << c[2] << endl << c[3] << endl;
 }
