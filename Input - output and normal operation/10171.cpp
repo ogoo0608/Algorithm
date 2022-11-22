cca3abbafcf395fa17dd3015e0d7a902389e2714
ogoo0608 <ogoo0608@gmail.com>
2022-11-22 10:39:34 +0000

    고양이

---
+++ Input - output and normal operation/10171.cpp
@@ -0,0 +1,26 @@
+/*
+문제
+아래 예제와 같이 고양이를 출력하시오.
+
+입력
+없음.
+
+출력
+고양이를 출력한다.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|";
+	
+	return 0;
+}
+
+// '\' 출력을 하려면 cout << "\\";
