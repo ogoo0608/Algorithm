0e3dabc81ea200ac011fd5d26de76f5bc655457b
ogoo0608 <ogoo0608@gmail.com>
2022-11-25 00:23:58 +0000

    새싹

---
+++ Input - output and normal operation/25083.cpp
@@ -0,0 +1,26 @@
+/*
+문제
+아래 예제와 같이 새싹을 출력하시오.
+
+입력
+입력은 없다.
+
+출력
+새싹을 출력한다.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	cout << "         ,r'\"\7\n";
+	cout << "r`-_   ,'  ,/\n";
+	cout << " \\. \". L_r\'\n";
+	cout << "   `~\\/\n";
+	cout << "      |\n";
+	cout << "      |";
+	
+	return 0;
+}
