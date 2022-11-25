d052323fc615fdf0bf02e6ca4f715df916446585
ogoo0608 <ogoo0608@gmail.com>
2022-11-25 00:27:33 +0000

    새싹

--- Input - output and normal operation/25083.cpp
+++ Input - output and normal operation/25083.cpp
@@ -15,9 +15,9 @@ using namespace std;
 
 int main() {
 	
-	cout << "         ,r'\"\7\n";
+	cout << "         ,r'\"7\n";
 	cout << "r`-_   ,'  ,/\n";
-	cout << " \\. \". L_r\'\n";
+	cout << " \\. \". L_r'\n";
 	cout << "   `~\\/\n";
 	cout << "      |\n";
 	cout << "      |";
